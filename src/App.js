ec64cfbd000d9d252e0d8da383dbd97298050ab6
leitediogo <leitediogo@gmail.com>
2016-09-21 11:50:05 +0100

    added some react componets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TextInput from './components/TextInput'
+import Button from './components/Button'
+import LikeButton from './components/LikeButton'
+import Avatar from './components/Avatar'
+import BelleComponents from './components/BelleComponents'
+//import EssenceComponents from './components/EssenceComponents'
+
 class App extends Component {
   render() {
     return (
@@ -11,8 +18,13 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Bora la começar com isto <code>src/App.js</code> and save to reload.
+          Bora la começar com isto nharr <code>src/App.js</code> and save to reload.
         </p>
+        <div><BelleComponents/></div>
+        <div><Button/></div>
+        <div><TextInput/></div>
+        <div><LikeButton/></div>
+        <div><Avatar/></div>
       </div>
     );
   }
