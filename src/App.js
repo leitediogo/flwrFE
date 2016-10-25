1f5dde457748b57e33dad84320c7d49e8dd8bbfe
leitediogo <leitediogo@Diogos-MacBook-Pro.local>
2016-10-25 23:31:17 +0100

    react-router added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import DecisionAppBar from './components/DecisionAppBar'
 import DecisionCardList from './components/DecisionCardList'
 import DecisionAddFloatingButton from './components/DecisionAddFloatingButton'
 
-
 // Needed for onTouchTap
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 let divstyle = { margin: 60 }
 
+function arrowTest(param1, param2) {
+  return param1 + param2
+}
+
+//arrow functions test
+const a = [
+  "obi",
+  "ani",
+  "luke",
+  "leia"
+];
+const alen = a.map(function (each) { return each + 'sa' });
+const alenarrow = a.map(each => each + 'aa\n');
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <p>{alenarrow}</p>
         <DecisionAppBar />
         <div><DecisionCardList /></div>
-        <DecisionAddFloatingButton/>
+        <DecisionAddFloatingButton />
         <div style={divstyle}><MaterialUITest /></div>
       </div>
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route, hashHistory } from 'react-router'
+import DecisionAppBar from './components/DecisionAppBar'
+import DecisionCardList from './components/DecisionCardList'
 
 ReactDOM.render(
-  <App />,
+  (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/appBar" component={DecisionAppBar}/>
+    <Route path="/cardList" component={DecisionCardList}/>
+  </Router>
+),
   document.getElementById('root')
 );
